f91d3761e778d6527774606b323e9d3560fdc471
dngKwngg <quangnguyenvk@gmail.com>
2025-03-31 17:27:50 +0700

    Add discovery server

---
+++ discovery-server/src/main/java/com/hrm/eureka/discovery/DiscoveryServerApplication.java
@@ -0,0 +1,16 @@
+package com.hrm.eureka.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+// Enable Eureka Server
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
