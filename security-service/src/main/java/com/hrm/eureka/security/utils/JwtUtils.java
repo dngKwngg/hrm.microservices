aec31945a5e8eb25e4922916b3a19e17c7d54649
dngKwngg <quangnguyenvk@gmail.com>
2025-04-02 12:10:28 +0700

    Add temp security service

---
+++ security-service/src/main/java/com/hrm/eureka/security/utils/JwtUtils.java
@@ -0,0 +1,25 @@
+package com.hrm.eureka.security.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+    private final String SECRET_KEY = "my-secret-key";
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().getSubject();
+    }
+}
